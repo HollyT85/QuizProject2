6bacd3d56311dba801d0a1ef55fc9d77dd3d579d
HollyT85 <h.e.turner@gmail.com>
2022-02-07 12:39:57 +0000

    added questions to JS

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+/*Array of objects for medium level questions*/
+
+let mediumQuestions= [
+    {
+        Question: "What animal did Queen Pasiphae sleep with before she gave birth to the minotaur?",
+        Options:['A white bull','A black cow', 'A golden ram','A brown goat' ],    
+        correctAnswer: 0
+    },
+    {
+        Question: "Who is the God of War in Polynesian mythology?",
+        Options: ['Ares','Ku','Zeus','Kratos'],    
+        correctAnswer: 1
+    },
+    {
+        Question: "In African mythology, Anansi is a trickster and storyteller who takes the shape of which animal?",
+        Options: ['Spider','Rabbit','Lizard','Dog'],
+        correctAnswer: 0
+    },
+    {
+        Question: "In Greek mythology, who killed Achilles?",
+        Options: ['Troy','Hera','Paris', 'Hermes'] ,     
+        correctAnswer: 2
+    },
+    {
+        Question: "Which of these Roman gods or goddesses doesn't have a Greek counterpart?",
+        Options: ['Venus','Mars','Jupiter','Janus'],
+        correctAnswer: 3
+    },
+    {
+        Question: "Hel was the daughter of which Norse god?",
+        Options:['Odin','Loki','Thor','Tyr'],      
+        correctAnswer: 1
+    },
+    {
+        Question: "What are the names of the first humans in Norse mythology?",
+        Options: ['Adam & Eve','Shu & Tefnut','Askr & Embla','Prometheus & Athena'],      
+        correctAnswer: 2
+    }, 
+    {
+        Question: "According to the Egyptian myth of Osiris, who murdered Osiris?",
+        Options: ['Anubis','Mina','Sekmet','Seth'],
+        correctAnswer: 3
+    },
+    {
+        Question: "Which of these is Hera goddess of?",
+        Options: ['Rivers','Fishing','Women','Children'],
+        correctAnswer: 3
+    },
+    {
+        Question: "In Chinese mythology, Changxi is the mother of what?",
+        
+            option1: 'Twelve moons',
+            option2: 'Twelve stars',
+            option3: 'Twelve planets',
+            option4: 'Twelve meteors',
+        
+        correctAnswer: 1
+    },
+    ]
