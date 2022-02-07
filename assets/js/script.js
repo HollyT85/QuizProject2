5d97f38d0e734db513347c6389505616c6ecf89b
HollyT85 <h.e.turner@gmail.com>
2022-02-07 18:19:12 +0000

    answers into HTML working correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,125 @@
 /* https://www.youtube.com/watch?v=QU6z69P5BrU&t=0s*/
 
-/*Array of objects for medium level questions*/
+//Array of objects for medium level questions
 
 const mediumQuestions= [
     {
         Question: "What animal did Queen Pasiphae sleep with before she gave birth to the minotaur?",
-        Options:['A white bull','A black cow', 'A golden ram','A brown goat' ],    
-        correctAnswer: 0
+        
+            option1: 'A white bull',
+            option2: 'A black cow',
+            option3: 'A golden ram',
+            option4: 'A brown goat',
+    
+        correctAnswer: 1
     },
     {
         Question: "Who is the God of War in Polynesian mythology?",
-        Options: ['Ares','Ku','Zeus','Kratos'],    
-        correctAnswer: 1
+        
+            option1: 'Ares',
+            option2: 'Ku',
+            option3: 'Zeus',
+            option4: 'Kratos',
+    
+        correctAnswer: 2
     },
     {
         Question: "In African mythology, Anansi is a trickster and storyteller who takes the shape of which animal?",
-        Options: ['Spider','Rabbit','Lizard','Dog'],
-        correctAnswer: 0
-    },
-    {
-        Question: "In Greek mythology, who killed Achilles?",
-        Options: ['Troy','Hera','Paris', 'Hermes'] ,     
-        correctAnswer: 2
-    },
-    {
-        Question: "Which of these Roman gods or goddesses doesn't have a Greek counterpart?",
-        Options: ['Venus','Mars','Jupiter','Janus'],
-        correctAnswer: 3
-    },
-    {
-        Question: "Hel was the daughter of which Norse god?",
-        Options:['Odin','Loki','Thor','Tyr'],      
+        
+            option1: 'Spider',
+            option2: 'Rabbit',
+            option3: 'Lizard',
+            option4: 'Dog',
+    
         correctAnswer: 1
     },
     {
-        Question: "What are the names of the first humans in Norse mythology?",
-        Options: ['Adam & Eve','Shu & Tefnut','Askr & Embla','Prometheus & Athena'],      
-        correctAnswer: 2
-    }, 
-    {
-        Question: "According to the Egyptian myth of Osiris, who murdered Osiris?",
-        Options: ['Anubis','Mina','Sekmet','Seth'],
+        Question: "In Greek mythology, who killed Achilles?",
+        
+            option1: 'Troy',
+            option2: 'Hera',
+            option3: 'Paris',
+            option4: 'Hermes',
+        
         correctAnswer: 3
     },
     {
-        Question: "Which of these is Hera goddess of?",
-        Options: ['Rivers','Fishing','Women','Children'],
+        Question: "Which of these Roman gods or goddesses doesn't have a Greek counterpart?",
+        
+            option1: 'Venus',
+            option2: 'Mars',
+            option3: 'Jupiter',
+            option4: 'Janus',
+        
+        correctAnswer: 4
+    },
+    {
+        Question: "Hel was the daughter of which Norse god?",
+        
+            option1: 'Zeus',
+            option2: 'Loki',
+            option3: 'Thor',
+            option4: 'Tyr',
+        
         correctAnswer: 2
     },
     {
+        Question: "What are the names of the first humans in Norse mythology?",
+        
+            option1: 'Adam & Eve',
+            option2: 'Shu & Tefnut',
+            option3: 'Askr & Embla',
+            option4: 'Prometheus & Athena',
+        
+        correctAnswer: 3
+    }, 
+    {
+        Question: "According to the Egyptian myth of Osiris, who murdered Osiris?",
+        
+            option1: 'Anubis',
+            option2: 'Mina',
+            option3: 'Sekmet',
+            option4: 'Seth',
+        
+        correctAnswer: 4
+    },
+    {
+        Question: "Which of these is Hera goddess of?",
+        
+            option1: 'Rivers',
+            option2: 'Fishing',
+            option3: 'Women',
+            option4: 'Children',
+        
+        correctAnswer: 3
+    },
+    {
         Question: "In Chinese mythology, Changxi is the mother of what?",
-        Options: ['Twelve moons', 'Twelve stars','Twelve planets','Twelve meteors'],
-        correctAnswer: 0
+        
+            option1: 'Twelve moons',
+            option2: 'Twelve stars',
+            option3: 'Twelve planets',
+            option4: 'Twelve meteors',
+        
+        correctAnswer: 1
     },
     ]
 
 
-/*Extracting IDs from medium level game*/
+//Extracting IDs from medium level game
 
 const updateQuestionNumber = document.getElementById('questionNumber');
 const question=document.getElementById('question');
-const option1=document.getElementById('option1');
-const option2=document.getElementById('option2');
-const option3=document.getElementById('option3');
-const option4=document.getElementById('option4');
+const answerContainers=Array.from(document.getElementsByClassName("answer-text"));
 
-
+//General quiz features
 let questionCounter=0;
 let numberOfQuestions=10;
 let currentQuestion;
 let availableQuestions=[];
 let availableAnswers=[];
 
+//Push available questions into an array
 function setAvailableQuestions () {
     const allQuestions=mediumQuestions.length;
     for (let i=0; i<allQuestions; i++){
@@ -79,28 +127,28 @@ function setAvailableQuestions () {
         } 
 }
 
+//set new questions, ensure they're not repeated, set answers and increase question counter
 function newQuestion () {
-
+    //questions
     const questionIndex=availableQuestions[Math.floor(Math.random()*availableQuestions.length)];
     currentQuestion=questionIndex;
     question.innerHTML=currentQuestion.Question;
     const index=availableQuestions.indexOf(questionIndex)
     availableQuestions.splice(index, 1);
 
-    /*answers*/
+    //answers
+    answerContainers.forEach(option => {
+        const number = option.dataset['number']
+        option.innerHTML = currentQuestion['option' + number]
+    })
 
-    const options=currentQuestion.Options;
-    console.log(options)
-        option1.innerHTML=options[0];
-        option2.innerHTML=options[1];
-        option3.innerHTML=options[2];
-        option4.innerHTML=options[3];
-
+    //increase question counter and user display
     questionCounter++;
     updateQuestionNumber.innerText = `${questionCounter} / ${numberOfQuestions}`;
 }
 
 
+//next button and what to do when clicked
 function next () {
     if (questionCounter == numberOfQuestions){
     return window.location.assign('endgame.html')
@@ -110,7 +158,7 @@ function next () {
     }
 }
 
-window.onload=function() {
-    setAvailableQuestions();
-    newQuestion();
-}
+
+//run functions
+setAvailableQuestions();
+newQuestion();
